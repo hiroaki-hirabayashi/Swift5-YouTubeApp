8fa07bb3f8e0529f913411c28896459bd3ce1f7e
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-06-18 17:44:05 +0900

    Initial Commit

---
+++ Swift5 YouTubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5 YouTubeApp
+//
+//  Created by 平林宏淳 on 2020/06/18.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
